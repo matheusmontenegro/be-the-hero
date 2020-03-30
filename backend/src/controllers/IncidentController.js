46f35d433e12680b8d96eb808c1b7c61bd91227d
Matheus Montenegro <matheusmontenegroo5@gmail.com>
2020-03-29 21:50:19 -0300

    Mobile da aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -7,7 +7,7 @@ module.exports = {
         const [count] = await connection('incidents').count();
                         
         const incidents = await connection('incidents')
-        .join('ongs', 'ong_id', '=', 'incidents.ong_id')
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
         .limit(5)
         .offset((page - 1) * 5)
         .select([
@@ -18,9 +18,9 @@ module.exports = {
             'ongs.city',
             'ongs.uf'
         ]);
-
-        response.header('X-Total-Count', count['count(*)']);
         
+        response.header('X-Total-Count', count['count(*)']);
+
         return response.json(incidents);
     },
     async create(request, response){
@@ -33,6 +33,7 @@ module.exports = {
             value,
             ong_id
         });
+
         return response.json({ id });
     },
     async delete(request, response){
